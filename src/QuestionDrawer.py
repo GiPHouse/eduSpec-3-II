cf5468b6d41a5bf0f7a65c6502f5227009776d9b
TheMusicianOne <140833057+TheMusicianOne@users.noreply.github.com>
2026-02-10 14:56:23 +0100

    Base structure is decided (#46)

---
+++ src/QuestionDrawer.py
@@ -0,0 +1,11 @@
+from Question import Question
+from MultipleChoiceQuestion import MultipleChoiceQuestion
+
+class QuestionDrawer():
+    
+    @staticmethod
+    def drawQuestion(current_question:Question):
+
+        if current_question is MultipleChoiceQuestion:
+            pass
+        
