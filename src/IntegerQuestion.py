dd1be0f9a2b326c773232dcafb3e8d67f0e425df
Rozemarie <rozemarie00@gmail.com>
2026-02-13 13:35:34 +0100

    Changing layout on IntegerQuestion and add to QuestionDrawer

--- src/IntegerQuestion.py
+++ src/IntegerQuestion.py
@@ -6,7 +6,7 @@ class IntegerQuestion(Question):
     def __init__(self, title:str, bodytext:str, correct_answer:tuple[int,int], feedbacks:List[str],imgpath:Optional[str]=None):
 
         #the correct integer should be in the range between the first and second int, given as a tuple
-        #feedbacks is as follows: [feedback for too small answer, feedback for right answer, feedback for too large answer]
+        #feedbacks is as follows: [feedback for right answer, feedback for too small answer, feedback for too large answer]
         assert (correct_answer[0] <= correct_answer[1])
         assert(len(feedbacks) == 3)
 
@@ -22,14 +22,14 @@ class IntegerQuestion(Question):
 
         if user_input < self.correct_answer[0]:
             isAnswerCorrect = False
-            ReturnFeedback = self.feedback(0)
+            ReturnFeedback = self.feedback(1)
         elif user_input > self.correct_answer[1]:
             isAnswerCorrect = False
             ReturnFeedback = self.feedback(2)
         else:
             isAnswerCorrect = True
-            ReturnFeedback = self.feedback(1)
-            
+            ReturnFeedback = self.feedback(0)
+
         return (isAnswerCorrect, ReturnFeedback)
 
     def feedback(self,input:int):
--- src/QuestionDrawer.py
+++ src/QuestionDrawer.py
@@ -1,8 +1,11 @@
+import streamlit as st
+
 from Question import Question
 from MultipleChoiceQuestion import MultipleChoiceQuestion
 from IntegerQuestion import IntegerQuestion
 
 class QuestionDrawer():
+    
     
     @staticmethod
     def drawQuestion(current_question:Question):
@@ -12,3 +15,15 @@ class QuestionDrawer():
         if current_question is IntegerQuestion:
             pass
         
+        with st.container():
+            st.title(current_question.title)
+            st.image(current_question.imgpath) #if imgpath is None it just does nothing?
+            st.text(current_question.bodytext)
+
+            #depending on question we have an input field or smth
+
+            #if st.button: check answer
+            st.button("Submit Answer")
+
+            st.button("Reset")
+
