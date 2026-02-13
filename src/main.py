6c9a6f9b069af75fcbc2bc5ff8f011d62490fd6e
Emir Oztekin <emiroztekin@Emirs-MacBook-Air.local>
2026-02-13 01:01:37 +0100

    Preliminary Implementation of Molecule Displays using PDB file format

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from MultipleChoiceQuestion import MultipleChoiceQuestion
 from QuestionDrawer import QuestionDrawer
+from MoleculeDisplay import MoleculeDisplay
 
 st.write("""
 Hello world
@@ -10,3 +11,13 @@ Hello world
 
 mcq = MultipleChoiceQuestion("hi",["aaa","bbb"],0,["true","false"])
 QuestionDrawer.drawQuestion(mcq)
+
+# 3D Molecule Display Section
+st.divider()
+st.header("3D Molecule Viewer")
+
+# Example 1: Display protein by PDB ID
+st.subheader("Protein Structure (PDB: 1CRN)")
+MoleculeDisplay.display_protein_by_id(pdb_id='1CRN', style='stick', height=500, width=800)
+MoleculeDisplay.display_molecule_from_file('src/neoNR8.pdb', style='stick', height=500, width=800)
+# Example 2: Display molecule from PDB string (water molecule)
