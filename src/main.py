f2a32e5df355aaa088ee24d762d6dfa30f661786
Sybren <101103395+SypTitan@users.noreply.github.com>
2026-02-13 21:58:27 +0100

    Revert main.py changes

--- src/main.py
+++ src/main.py
@@ -1,21 +1,8 @@
-from MultipleChoiceQuestion import MultipleChoiceQuestion
-from Question import Question
-from QuestionSerialiser import QuestionSerialiser
+import streamlit as st
 
-# st.write("""
-# Hello world
-# """)
+st.write("""
+Hello world
+""")
 
-mcq = MultipleChoiceQuestion("question1", "title", "hi", ["aaa", "bbb"], 0, ["true", "false"])
-# mcq = MultipleChoiceQuestion("title", "hi", ["aaa", "bbb"], 0, ["true", "false"])
+# mcq = MultipleChoiceQuestion("title","hi",["aaa","bbb"],0,["true","false"])
 # QuestionDrawer.drawQuestion(mcq)
-# print(type(mcq)==MultipleChoiceQuestion)
-match mcq.__class__:
-    case Question():
-        print("Uh-oh")
-    case MultipleChoiceQuestion():
-        print("true")
-    case n:
-        print(n)
-# QuestionDrawer.drawQuestion(mcq)
-print(QuestionSerialiser.questionToJson(mcq))
