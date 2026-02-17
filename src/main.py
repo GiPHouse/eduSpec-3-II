8ae48870bd3764ebedb2a301c44d7031c0c6a9ef
TheMusicianOne <140833057+TheMusicianOne@users.noreply.github.com>
2026-02-17 16:30:38 +0100

    img visualization done (#69)

--- src/main.py
+++ src/main.py
@@ -3,12 +3,21 @@ from MultipleChoiceQuestion import MultipleChoiceQuestion
 from QuestionDrawer import QuestionDrawer
 
 questionint = IntegerQuestion(
-    "title", "this is the body text", (10, 20), ["correct", "wrong too small", "wrong too big"]
+    "title",
+    "this is the body text",
+    (10, 20),
+    ["correct", "wrong too small", "wrong too big"],
+    imgpath="../data/test.png",
 )
-# QuestionDrawer.drawQuestion(questionint)
+QuestionDrawer.drawQuestion(questionint)
 
 questionmult = MultipleChoiceQuestion(
-    "title", "this is the body text", ["10", "20", "30", "40"], 2, ["a", "b", "c", "d"]
+    "title",
+    "this is the body text",
+    ["10", "20", "30", "40"],
+    2,
+    ["a", "b", "c", "d"],
+    imgpath="../data/test.png",
 )
 
-QuestionDrawer.drawQuestion(questionmult)
+# QuestionDrawer.drawQuestion(questionmult)
