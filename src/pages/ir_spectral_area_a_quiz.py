0903fd248e21ee83c220f03b18ed62e30ccec19e
Soykan Yildizdogan <soykanyildizdogan@gmail.com>
2026-02-20 16:57:27 +0100

    housekeeping

--- src/pages/ir_spectral_area_a_quiz.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-from components.navigation import showIrSidebar, showNavbar
-
-showNavbar()
-st.title("Mini Quiz \n Area A (3800 - 3200 cm-1)")
-st.write("Welcome to the IR Spectral Areas A Page!")
-showIrSidebar()
