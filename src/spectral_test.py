d674eca58f19aaef9caa441a15b587820e86abd7
Yusuf Hilo <ya.hilooglu@gmail.com>
2026-02-20 15:17:37 +0100

    adjusted spectralquestion class to fit in new design

---
+++ src/spectral_test.py
@@ -0,0 +1,6 @@
+from QuestionDrawer import QuestionDrawer
+from SpectralQuestion import SpectralQuestion
+
+spectralquest = SpectralQuestion("title", "this is the body text", "../spectra/easy001/ms.dx")
+
+QuestionDrawer.drawQuestion(spectralquest)
