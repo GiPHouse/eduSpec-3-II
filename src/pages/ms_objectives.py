bfec6e72fe35dfc6a40023200bd93f6becedb188
jolienborst <jolien.borst@ru.nl>
2026-02-27 15:51:04 +0100

    New navigation (deleted pages and outdated navigation) (#93)
    Co-authored-by: Jolien <jolientien@gmail.com>

--- src/pages/ms_objectives.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-from components.navigation import showNavbar
-
-showNavbar()
-st.title("MS")
-st.write("Welcome to the MS Page!")
--- src/pages/using_eduspec.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-from components.navigation import showNavbar
-
-showNavbar()
-st.title("Using Eduspec")
-st.write("Welcome to the Using Eduspec Page!")
