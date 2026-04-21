ed38baa7c48b74476169c0ba8bf4afe0a3774c24
Yusuf Hilo <ya.hilooglu@gmail.com>
2026-04-21 15:49:15 +0200

    bug fixes with editor preview and spectral questions

--- src/QuestionDrawer.py
+++ src/QuestionDrawer.py
@@ -35,7 +35,7 @@ class QuestionDrawer:
             current_question.drawImage()
             # Check if we have a spectral question: in that case create a download button with _drawDownload
             if isinstance(current_question, SpectralQuestion):
-                QuestionDrawer.drawDownload(current_question)
+                QuestionDrawer._drawDownload(current_question)
             st.text(current_question.bodytext)
 
             with st.form("form" + current_question.title, enter_to_submit=False):
@@ -44,12 +44,21 @@ class QuestionDrawer:
                     if user_input is not None:
                         QuestionDrawer.evaluateAnswer(current_question, user_input)
 
+            def _handle_reset_drawing_question() -> None:
+                nonce_key = f"{current_question.widget_key}__jsme_nonce"
+                last_seen_key = f"{current_question.widget_key}__last_seen"
+                if nonce_key in st.session_state:
+                    st.session_state[nonce_key] += 1
+                if last_seen_key in st.session_state:
+                    st.session_state[last_seen_key] = current_question.default
+
             def _reset_callback() -> None:
                 st.session_state[current_question.widget_key] = current_question.default
+                _handle_reset_drawing_question()
 
             st.button("Reset", on_click=_reset_callback)
             # st.rerun()
-            
+
     @staticmethod
     @st.fragment  # This is a fragment so the app doesn't rerun when clicking the download button
     def _drawDownload(current_question: Question) -> None:
@@ -64,5 +73,8 @@ class QuestionDrawer:
         """
         with open(current_question.imgpath) as f:
             st.download_button(
-                "Download Spectral Data", f, file_name=os.path.basename(current_question.imgpath), icon=":material/file_download:"
+                "Download Spectral Data",
+                f,
+                file_name=os.path.basename(current_question.imgpath),
+                icon=":material/file_download:",
             )
